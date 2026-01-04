60eeb7ba0f6c4aa4256a9b2ded37b8822fb3b7e7
Arun03-Perumal <arun03.perumal@gmail.com>
2026-01-04 21:34:25 +0530

    Update API base URL for production

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://library-backend-qzqr.onrender.com/api"
 });
 
+// Get all books
 export const getBooks = () => API.get("/books");
+
+// Add book
 export const addBook = (book) => API.post("/books", book);
-export const deleteBook = (id) => API.delete(`/books/${id}`);
+
+// Update book
 export const updateBook = (id, book) => API.put(`/books/${id}`, book);
+
+// Delete book
+export const deleteBook = (id) => API.delete(`/books/${id}`);
